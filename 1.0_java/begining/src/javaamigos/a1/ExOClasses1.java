81724a3c0b3789e62706848011aaf27dd570dc0e
luis31 <luismunozc3@gmail.com>
2023-06-29 16:18:49 -0500

    Util lesson 140

--- 1.0_java/begining/src/javaamigos/a1/ExOClasses1.java
+++ 1.0_java/begining/src/javaamigos/a1/ExOClasses1.java
@@ -2,15 +2,28 @@ package javaamigos.a1;
 
 public class ExOClasses1 {
     public static void main(String[] args) {
-        ExOClassesCat rose = new ExOClassesCat();
-        rose.setName("Rose");
+        ExOClassesCat rose = new ExOClassesCat("Rose", 5, "blue");
         rose.meow();
+        System.out.println(rose.getName());
+        System.out.println(rose.getAge());
 
-        ExOClassesCat star = new ExOClassesCat();
-        star.setName("Star");
-        star.meow();
+        //
+        System.out.println(rose);
 
-    }
+//        System.out.println();
+//
+//        ExOClassesCat star = new ExOClassesCat("Star",3);
+//        star.meow();
+//        System.out.println(star.getName());
+//        System.out.println(star.getAge());
+//
+//        System.out.println();
+//
+//        ExOClassesCat jupiter = new ExOClassesCat("jupiter");
+//        System.out.println(jupiter.getName());
+//        System.out.println(jupiter.getAge());
+//
+   }
 
 
 }
--- 1.0_java/begining/src/javaamigos/a1/ExOClassesCat.java
+++ 1.0_java/begining/src/javaamigos/a1/ExOClassesCat.java
@@ -2,6 +2,22 @@ package javaamigos.a1;
 
 public class ExOClassesCat {
     private String name;
+    private String color;
+    private int age;
+
+    public ExOClassesCat() {
+    }
+
+    public ExOClassesCat(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public ExOClassesCat(String name){
+        this.name = name;
+        this.age = 0;
+    }
 
     public void meow() {
         System.out.println(name + " : meow...");
@@ -9,5 +25,29 @@ public class ExOClassesCat {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String toString(){
+        return "name: " + name + " Color: " + color + " Age: " + age;
     }
 }
